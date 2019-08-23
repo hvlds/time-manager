4245b955845ac2170f0f2d07533c7464b6348e0e
Hernán Valdés <hfvaldesg@gmail.com>
2019-08-23 14:55:34 +0200

    Add clean edit text functionality

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import urwid
 
+def on_clean_clicked(txt, button):
+    txt.set_edit_text("")
+
 def main():
     txt = urwid.Edit((""))
-    txt = urwid.LineBox(txt, "Task description")
+    txt_boxed = urwid.LineBox(txt, "Task description")
     header = urwid.Text("Time Manager", align="center")
+    start = urwid.Button("start")
+    clean = urwid.Button("clean")
     buttons = urwid.Columns([
-        urwid.Button("save"),
-        urwid.Button("clean"),
+        start,
+        clean,
     ])
     widget = urwid.Pile([
         header,
-        txt,
+        txt_boxed,
         buttons,
     ])
     widget = urwid.Padding(widget, align='center', width=('relative', 85))
     widget = urwid.Filler(widget, "middle")
+
+    urwid.connect_signal(clean, "click", on_clean_clicked, weak_args=[txt])
     loop = urwid.MainLoop(widget)
     loop.run()
 
