21828d2aefb900cd2602f4bc3e47954f573525ab
Hernán Valdés <hfvaldesg@gmail.com>
2019-08-23 13:41:16 +0200

    Testing UI with urwid. Create basic template, still no functionality

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 import urwid
 
-def exit_on_q(key):
-    if key in ('q', 'Q'):
-        raise urwid.ExitMainLoop()
+def main():
+    txt = urwid.Edit((""))
+    txt = urwid.LineBox(txt, "Task description")
+    header = urwid.Text("Time Manager", align="center")
+    buttons = urwid.Columns([
+        urwid.Button("save"),
+        urwid.Button("clean"),
+    ])
+    widget = urwid.Pile([
+        header,
+        txt,
+        buttons,
+    ])
+    widget = urwid.Padding(widget, align='center', width=('relative', 85))
+    widget = urwid.Filler(widget, "middle")
+    loop = urwid.MainLoop(widget)
+    loop.run()
 
-palette = [
-    ('banner', '', '', '', '#ffa', '#60d'),
-    ('streak', '', '', '', 'g50', '#60a'),
-    ('inside', '', '', '', 'g38', '#808'),
-    ('outside', '', '', '', 'g27', '#a06'),
-    ('bg', '', '', '', 'g7', '#d06'),]
-
-placeholder = urwid.SolidFill()
-loop = urwid.MainLoop(placeholder, palette, unhandled_input=exit_on_q)
-loop.screen.set_terminal_properties(colors=256)
-loop.widget = urwid.AttrMap(placeholder, 'bg')
-loop.widget.original_widget = urwid.Filler(urwid.Pile([]))
-
-div = urwid.Divider()
-outside = urwid.AttrMap(div, 'outside')
-inside = urwid.AttrMap(div, 'inside')
-txt = urwid.Text(('banner', u" Hello World "), align='center')
-streak = urwid.AttrMap(txt, 'streak')
-pile = loop.widget.base_widget # .base_widget skips the decorations
-for item in [outside, inside, streak, inside, outside]:
-    pile.contents.append((item, pile.options()))
-
-loop.run()
+if __name__ == "__main__":
+    main()
