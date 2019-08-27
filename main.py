886f52b6a5a0eb8395da4135816c26113cf41e91
Hernán Valdés <hfvaldesg@gmail.com>
2019-08-27 12:53:58 +0200

    add basic functionality to start button through alarms in buttons

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import urwid
+from datetime import datetime
 
 def on_clean_clicked(txt, button):
     txt.set_edit_text("")
 
-def main():
+def refresh(loop, data=None):
+    header.set_text(str(datetime.now()))
+    loop.set_alarm_in(1, refresh)
+
+def on_start_clicked(header, loop, button):
+    refresh(loop)
+
+if __name__ == "__main__":
     txt = urwid.Edit((""))
     txt_boxed = urwid.LineBox(txt, "Task description")
     header = urwid.Text("Time Manager", align="center")
@@ -23,7 +31,6 @@ def main():
 
     urwid.connect_signal(clean, "click", on_clean_clicked, weak_args=[txt])
     loop = urwid.MainLoop(widget)
+    # loop.set_alarm_in(1, refresh)
+    urwid.connect_signal(start, "click", on_start_clicked, weak_args=[header,loop])
     loop.run()
-
-if __name__ == "__main__":
-    main()
